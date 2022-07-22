2f3262cb3d28ed876c017d07b680fa3f1dd9e9ed
TritonHalley <Triton.halley@outlook.com>
2022-07-22 19:11:49 +0430

    add search events component to filter event by date

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,20 @@
+import { useRouter } from "next/router";
 import { getAllEvents } from "../../DUMMY_DATA";
 import EventList from "../../Components/Events/event-list";
+import EventsSearch from "../../Components/Events/events-search";
 const AllEvents = () => {
+  const router = useRouter();
   const events = getAllEvents();
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    console.log(fullPath);
+    router.push(fullPath);
+  }
+
   return (
     <>
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
