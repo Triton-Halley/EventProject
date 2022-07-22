37b58cdc58a13a6b8af633e5d4c20c61713aa7f2
TritonHalley <Triton.halley@outlook.com>
2022-07-22 10:44:47 +0430

    Add basics  files for events project

---
+++ pages/events/[eventId].js
@@ -0,0 +1,8 @@
+const EventDetailPage = () => {
+  return (
+    <div>
+      <h1>this Event Detail page</h1>
+    </div>
+  );
+};
+export default EventDetailPage;
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+const AllEvents = () => {
+  return (
+    <div>
+      <h1>All Event</h1>
+    </div>
+  );
+};
+
+export default AllEvents;
